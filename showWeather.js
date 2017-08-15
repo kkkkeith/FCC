aa5d53dcfcfd23428d3cbd69cc6517d4f83c4a79
keith <keithshy@163.com>
2017-08-15 19:55:56 +0800

    Add files via upload

---
+++ showWeather.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+	// var weather;
+	$('button').click(function ( ) {
+		var name = encodeURI($('input').val());
+		//解决跨域问题,直接得到js对象
+		$.getJSON('http://v.juhe.cn/weather/index?type=jsonp&format=2&cityname='+name+'&key=853b9cc74bf35afd44b12b7486951c2a&callback=?', function (data,state) {
+		      var sk = data.result.sk; 
+                  var today = data.result.today; 
+                  var future = data.result.future;
+                  var arr = [];
+                  var arrToday = [];
+                  var arrFuture = [];
+                  for (var x in sk) {
+            	     arr.push(sk[x]);
+                  }
+                  for (var x in today) {
+                        arrToday.push(today[x]);
+                  }
+                  arrToday.splice(2,1);
+                  for (var i = 0; i < future.length; i++) {
+                        for (var j in future[i]) {
+                              arrFuture.push(future[i][j]);
+                        }
+                  }
+                  for (var n = 2; n < arrFuture.length; n += 5) {
+                        arrFuture.splice(n,1);
+                  }
+                  for (var i = 0; i < arr.length; i++) {
+            	     //注意i的位置,要这样遍历
+            	     $('table:eq(0) tr:eq('+i+') td:eq(1)').text(arr[i]);
+                  }
+                  for (var i = 0; i < arrToday.length; i++) {
+                        $('table:eq(1) tr:eq('+i+') td:eq(1)').text(arrToday[i]);
+                  }
+                  for (var i = 0; i < arrFuture.length; i++) {
+                        $('table:eq(2) tr:eq('+i+') td:eq(1)').text(arrFuture[i]);
+                  }
+                  $('table:eq(2) tr:even').css('color','#03c');
+	    });
+	});
+});
