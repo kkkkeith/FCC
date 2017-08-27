49a77ff8ae89fe33c036e08efaa50ee7af179d98
keith <keithshy@163.com>
2017-08-27 17:06:27 +0800

    Update showWeather.js

--- showWeather.js
+++ showWeather.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 		var name = encodeURI($('input').val());
 		//解决跨域问题,直接得到js对象(https解决混合内容页面)
 		$.getJSON('https://v.juhe.cn/weather/index?type=jsonp&format=2&cityname='+name+'&key=853b9cc74bf35afd44b12b7486951c2a&callback=?', function (data,state) {
-		  var sk = data.result.sk; 
+		      var sk = data.result.sk; 
                   var today = data.result.today; 
                   var future = data.result.future;
                   var arr = [];
@@ -26,10 +26,10 @@ $(document).ready(function () {
                   }
                   for (var i = 0; i < arr.length; i++) {
             	     //注意i的位置,要这样遍历
-            	     $('table:eq(0) tr:eq('+i+') td:eq(1)').text(arr[i]);
+            	     $('table:eq(1) tr:eq('+i+') td:eq(1)').text(arr[i]);
                   }
                   for (var i = 0; i < arrToday.length; i++) {
-                        $('table:eq(1) tr:eq('+i+') td:eq(1)').text(arrToday[i]);
+                        $('table:eq(0) tr:eq('+i+') td:eq(1)').text(arrToday[i]);
                   }
                   for (var i = 0; i < arrFuture.length; i++) {
                         $('table:eq(2) tr:eq('+i+') td:eq(1)').text(arrFuture[i]);
@@ -37,6 +37,7 @@ $(document).ready(function () {
                   for (var index = 0; index < arrFuture.length; index = index + 5) {
                        $('table:eq(2) tr:eq('+index+')').css('color','#03c');
                   }
+
                   
 	    });
 	});
