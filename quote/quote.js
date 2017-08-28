2c60083c9a65b3276dac8f0210eafee188496b08
keith <keithshy@163.com>
2017-08-28 15:36:27 +0800

    Update quote.js

--- quote/quote.js
+++ quote/quote.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
          })("");
     }
 	$('button').click(function () {
-		$.getJSON('https://sslapi.hitokoto.cn/?encode=json&callback=?', function (data) {
+		$.getJSON('https://sslapi.hitokoto.cn/?encode=json', function (data) {
 			qContent = data.hitokoto;
             qAuthor = data.from; 
             author.html("—— " + qAuthor);
