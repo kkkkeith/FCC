d8d61d3bd22cf5abcc27715316b44d84c5a0697c
keith <keithshy@163.com>
2017-08-28 18:34:18 +0800

    Update quote.js

--- quote/quote.js
+++ quote/quote.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
 	$('button').click(function () {
 		var noCache = Date();
 		$.getJSON('https://sslapi.hitokoto.cn/?encode=json', {"noCache":noCache}, function (data) {
-		//$.getJSON('https://sslapi.hitokoto.cn/?encode=json', function (data) {
 			qContent = data.hitokoto;
                     	qAuthor = data.from; 
                 	author.html("—— " + qAuthor);
@@ -25,27 +24,27 @@ $(document).ready(function () {
             		$('body').css('backgroundColor', color);
             		newQuote.css('backgroundColor', color);
             		qq.css('backgroundColor', color);
-           		 weibo.css('backgroundColor', color);
+           		weibo.css('backgroundColor', color);
             		left.css({'left':'0','color':color});
-           		 text.css('color', color);
+           		text.css('color', color);
 		});
 	});
 	function qqShare(content, author) {    
-        var shareqqzonestring = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?summary='+content+author;  
-        window.open(shareqqzonestring,'newwindow','height=400,width=400,top=100,left=100');
-    }
-    function weiboShare(content, author) {
-        var sharesinastring = 'http://v.t.sina.com.cn/share/share.php?title='+content+author;  
-        window.open(sharesinastring,'newwindow','height=400,width=400,top=100,left=100'); 
-    }
+            var shareqqzonestring = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?summary='+content+author;  
+            window.open(shareqqzonestring,'newwindow','height=400,width=400,top=100,left=100');
+        }
+        function weiboShare(content, author) {
+            var sharesinastring = 'http://v.t.sina.com.cn/share/share.php?title='+content+author;  
+            window.open(sharesinastring,'newwindow','height=400,width=400,top=100,left=100'); 
+        }
 	qq.click(function () {
-        var getContent = content.html();
-        var getAuthor = author.html();
-        qqShare(getContent, getAuthor);
-    });
-    weibo.click(function () {
-        var getContent = content.html();
-        var getAuthor = author.html();
-        weiboShare(getContent, getAuthor);
-    });
+            var getContent = content.html();
+            var getAuthor = author.html();
+            qqShare(getContent, getAuthor);
+        });
+        weibo.click(function () {
+            var getContent = content.html();
+            var getAuthor = author.html();
+            weiboShare(getContent, getAuthor);
+        });
 });
